23e34b12d244e81854d2e5b3d472ae62eb42cd6c
JeongYunMi <tamd5971@gmail.com>
2024-08-26 08:13:58 +0900

    feat: 로그인 UI 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import Secen from "../component/Secen";
 import { OrbitControls } from "@react-three/drei";
+import { useCallback, useState } from "react";
+import Index from "../component/account/Index";
 
 function Home() {
+  const [showLoginModal, setShowLoginModal] = useState<boolean>(false)
+
+  const chairClick = useCallback(()=>{
+    setShowLoginModal(prev=>!prev)
+  },[])
+
   return (
-    <Canvas shadows>
-      <Secen />
-      <OrbitControls />
-    </Canvas>
+    <>
+      <Canvas shadows>
+        <Secen loginModalOpenHdr={chairClick} />
+        <OrbitControls  />
+      </Canvas>
+      {showLoginModal && <Index onClose={chairClick}/>}
+    </>
   );
 }
 
