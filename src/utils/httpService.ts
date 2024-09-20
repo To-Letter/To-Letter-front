f9329113f213d5a8d14f6ce38011dbfc0300dd23
JeongYunMi <tamd5971@gmail.com>
2024-09-20 17:52:51 +0900

    chore: 코드 주석 추가 #17

--- src/utils/httpService.ts
+++ src/utils/httpService.ts
@@ -5,6 +5,7 @@ const authorization = () => {
     headers: {
       Authorization: `${sessionStorageService.get("accessToken")}`,
       refreshToken: `${sessionStorageService.get("refreshToken")}`,
+      // 아래는 cors 이슈 확인 후 추가한 구문으로, 효과 X
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Credentials':true,
       'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,OPTIONS',
--- src/utils/sessionStorageService.ts
+++ src/utils/sessionStorageService.ts
@@ -1,6 +1,17 @@
 import { SESSION_ACCESSTOKEN_KEY, SESSION_REFRESHTOKEN_KEY } from "../constants/session";
+// SESSION_ACCESSTOKEN_KEY : 엑세스 토큰 key 값 상수 문자열 -'JWTAccountAccessToken'
+// SESSION_REFRESHTOKEN_KEY : 리프레시 토큰 key 값 상수 문자열 -'JWTAccountRefresToken'
 
+/**
+ * 세션 스토리지 관리
+ */
 const sessionStorageService = {
+  /**
+   * 세션 스토리지에 값을 넣는 메소드
+   * @param type "accessToken" | "refreshToken"
+   * @param value 받아온 토큰 값
+   * @returns type과 value를 통해 세션 스토리지에 각각의 토큰을 저장
+   */
   set: (type: "accessToken" | "refreshToken", value: string = "") => {
     if(typeof window !== "undefined"){
       if(type === "accessToken"){
@@ -10,6 +21,11 @@ const sessionStorageService = {
       }
     }
   },
+  /**
+   * 세션 스토리지에서 값을 가져오는 메소드
+   * @param type "accessToken" | "refreshToken"
+   * @returns type에 맞는 토큰을 반환, 로그인하지 않았을 경우 null
+   */
   get: (type: "accessToken" | "refreshToken") => {
     if(typeof window !== "undefined"){
     const data = type ==="accessToken" 
@@ -22,6 +38,11 @@ const sessionStorageService = {
       return  null;
     }
   },
+
+  /**
+   * 세션 스토리지에서 각각의 토큰을 삭제하는 메소드
+   * @returns accessToken, refreshToken 토큰 삭제
+   */
   delete: () => {
     return typeof window !== "undefined"
       ? (sessionStorage.removeItem(SESSION_ACCESSTOKEN_KEY)
