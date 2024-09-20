2c21153a1a863ae3216dcc60bd06fdf262269961
JeongYunMi <tamd5971@gmail.com>
2024-09-20 17:13:13 +0900

    feat: 백엔드 api 통신 구조 테스트를 위한 api 통신 코드 추가 #11, #15

---
+++ src/apis/controller/account.ts
@@ -0,0 +1,36 @@
+import sendApi from "../sendApi";
+import sessionStorageService from "../../utils/sessionStorageService";
+
+export const postLocalLogin = async (loginData: { email: string; password?: string })  => {
+  const response: any = await sendApi.post(`/users/su/login`, {
+    email: loginData.email,
+    password: loginData.password,
+  });
+
+  const accessToken = response.headers.get("authorization");
+  const refreshToken = response.headers.get("refreshtoken");
+
+  sessionStorageService.set("accessToken", accessToken);
+  sessionStorageService.set("refreshToken", refreshToken);
+  return response;
+};
+
+export const getReissue = async ()  => {
+  const response: any = await sendApi.get(`/users/reissue`);
+
+  const accessToken = response.headers.get("authorization");
+  const refreshToken = response.headers.get("refreshtoken");
+  
+  sessionStorageService.set("accessToken", accessToken);
+  sessionStorageService.set("refreshToken", refreshToken);
+
+  return response;
+};
+
+export const getLogout = async ()  => {
+  const response: any = await sendApi.get(`/users/logout`);
+
+  sessionStorageService.delete();
+
+  return response;
+};
