40f201c4d2cb3779cc4d1cc4dc76526d233e0786
kimjeyoun <wodbs5602@naver.com>
2024-09-25 16:16:39 +0900

    feat: 회원가입 api 구현중 #15

--- src/apis/controller/account.ts
+++ src/apis/controller/account.ts
@@ -1,7 +1,10 @@
 import sendApi from "../sendApi";
 import sessionStorageService from "../../utils/sessionStorageService";
 
-export const postLocalLogin = async (loginData: { email: string; password?: string })  => {
+export const postLocalLogin = async (loginData: {
+  email: string;
+  password?: string;
+}) => {
   const response: any = await sendApi.post(`/users/su/login`, {
     email: loginData.email,
     password: loginData.password,
@@ -16,19 +19,58 @@ export const postLocalLogin = async (loginData: { email: string; password?: stri
   return response;
 };
 
-export const getReissue = async ()  => {
+// 회원가입
+export const postLocalSignup = async (signupData: {
+  nickname: string;
+  email: string;
+  loginType: string;
+  password: string;
+  address: string;
+}) => {
+  const response: any = await sendApi.post(`/users/su/signup`, {
+    nickname: signupData.nickname,
+    email: signupData.email,
+    loginType: signupData.loginType,
+    password: signupData.password,
+    address: signupData.address,
+  });
+
+  return response;
+};
+
+// 닉네임 중복확인
+export const getNicknameConfirm = async (nickname: { nickname: string }) => {
+  const queryString = `?userNickname=${encodeURIComponent(nickname.nickname)}`;
+  const response: any = await sendApi.get(
+    `/users/su/confirmNickname${queryString}`
+  );
+
+  return response;
+};
+
+// 이메일 중복확인
+export const getEmialConfirm = async (email: { email: string }) => {
+  const queryString = `?userEmail=${encodeURIComponent(email.email)}`;
+  const response: any = await sendApi.get(
+    `/users/su/confirmEmail${queryString}`
+  );
+
+  return response;
+};
+
+export const getReissue = async () => {
   const response: any = await sendApi.get(`/users/reissue`);
 
   const accessToken = response.headers.get("authorization");
   const refreshToken = response.headers.get("refreshtoken");
-  
+
   sessionStorageService.set("accessToken", accessToken);
   sessionStorageService.set("refreshToken", refreshToken);
 
   return response;
 };
 
-export const getLogout = async ()  => {
+export const getLogout = async () => {
   const response: any = await sendApi.get(`/users/logout`);
 
   sessionStorageService.delete();
