a64f7bc1b4e91299fc00ca1aebbcf8c05c024116
kimjeyoun <wodbs5602@naver.com>
2024-11-01 15:08:41 +0900

    feat: 편지 보내기 기능 구현 #39

--- src/recoil/letterPopupAtom.ts
+++ src/recoil/letterPopupAtom.ts
@@ -1,7 +1,17 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 // 로딩 상태를 관리하는 Recoil atom
 export const letterPopupState = atom<boolean>({
-  key: 'letterPopupState', // 고유 키
+  key: "letterPopupState", // 고유 키
   default: false, // 초기값 false
+});
+
+export const sendLetterModalState = atom<boolean>({
+  key: "sendLetterModalState", // 고유 키
+  default: false, // 초기값 false
+});
+
+export const letterContentState = atom<string>({
+  key: "letterContentState", // 고유 키
+  default: "", // 초기값 false
 });
