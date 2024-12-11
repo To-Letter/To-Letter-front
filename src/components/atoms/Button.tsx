01afd18d19e32b2454e0c3f1e3046ccd363eb028
JeongYunMi <tamd5971@gmail.com>
2024-12-12 05:03:20 +0900

    pref: Atoms Component(*button(modi comment, width style props add)) #86

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -4,12 +4,14 @@ import styled from "styled-components";
 interface props {
   title: string;
   onClick: () => void;
+  $width?: string;
   $fontSize?: string;
   $padding?: string;
   $margin?: string;
 }
 
 interface defaultStyleProps {
+  $width?: string;
   $fontSize?: string;
   $padding?: string;
   $margin?: string;
@@ -18,14 +20,16 @@ interface defaultStyleProps {
 /**
  * @param title 버튼 TEXT(String)
  * @param onClick onClick event(()=>void)
- * @param $fontSize fontSize Style(String)
- * @param $margin margin Style(String)
- * @param $padding padding Style(String)
+ * @param $width? width Style(String), 기본 값 "100%"
+ * @param $fontSize? fontSize Style(String), 기본 값 "16px"
+ * @param $margin? margin Style(String), 기본 값 "0"
+ * @param $padding? padding Style(String), 기본 값 "0"
  * @returns 기본 버튼 디자인 UI
  */
 export default function Button({
   title,
   onClick,
+  $width = "100%",
   $fontSize = "16px",
   $margin = "0",
   $padding = "0",
@@ -33,6 +37,7 @@ export default function Button({
   return (
     <ButtonWrap
       onClick={onClick}
+      $width={$width}
       $fontSize={$fontSize}
       $margin={$margin}
       $padding={$padding}
@@ -43,7 +48,7 @@ export default function Button({
 }
 
 const ButtonWrap = styled.button<defaultStyleProps>`
-  width: 100%;
+  width: ${({ $width }) => $width};
   border: 1px solid #e9e9e9;
   display: flex;
   justify-content: center;
@@ -54,4 +59,5 @@ const ButtonWrap = styled.button<defaultStyleProps>`
   color: #e9e9e9;
   background-color: #262523;
   border-radius: 1px;
+  cursor: pointer;
 `;
