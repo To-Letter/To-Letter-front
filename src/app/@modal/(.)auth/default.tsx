c1804a18994a582d586e353e702be2eaa9ff4211
JeongYunMi <tamd5971@gmail.com>
2024-12-12 04:17:39 +0900

    perf: /auth default.tsx 추가 #87

---
+++ src/app/@modal/(.)auth/default.tsx
@@ -0,0 +1,9 @@
+// auth/default.tsx
+import { redirect } from "next/navigation";
+
+/**
+ * /auth로 접근 시 /auth/login으로 리다이렉트
+ */
+export default function AuthDefault() {
+  redirect("/auth/login");
+}
