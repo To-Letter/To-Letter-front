3272d95a1aee1bb6b4fca2f12bacd9b22eaac3b2
JeongYunMi <tamd5971@gmail.com>
2024-12-12 15:03:51 +0900

    fix: client component 변경 #88

--- src/components/atoms/Box.tsx
+++ src/components/atoms/Box.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styled, { css } from "styled-components";
 
 interface defaultStyleProps {
--- src/components/molecules/AuthMenuTab.tsx
+++ src/components/molecules/AuthMenuTab.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Tab from "@/components/atoms/Tab";
 import { usePathname, useRouter } from "next/navigation";
