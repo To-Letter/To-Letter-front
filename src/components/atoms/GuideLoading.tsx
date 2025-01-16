51cf0ae8097ee4c5ae3a692295dacb3b556b0d70
JeongYunMi <tamd5971@gmail.com>
2025-01-16 13:27:28 +0900

    feat: 로딩 퍼센트 및 루트 페이지 이동 버튼 추가 #66

---
+++ src/components/atoms/GuideLoading.tsx
@@ -0,0 +1,118 @@
+"use client";
+import { useModelLoadingStore } from "@/store/recoil/zustand/modelLoadingStore";
+import React from "react";
+import { keyframes, styled } from "styled-components";
+import { Text } from "./Text";
+import Image from "next/image";
+import { ElementBox, SectionBox } from "./Box";
+import { useRouter } from "next/navigation";
+
+export default function GuideLoading() {
+  const router = useRouter();
+  const { progress } = useModelLoadingStore();
+
+  return (
+    <Overlay
+      $width="200px"
+      $height="100px"
+      $justifyContent="flex-end"
+      $alignItems="center"
+      $margin="8px"
+      $borderRadius="8px"
+      $backgroundColor="rgba(0, 0, 0, 0.5)"
+    >
+      <Spinner>
+        <Text $color="white" $fontWeight="bold">
+          {Math.round(progress)}%
+        </Text>
+      </Spinner>
+      <ElementBox $margin="0 16px 0 0">
+        {progress >= 100 && (
+          <MoveRootPage onClick={() => router.push("/")}>
+            <Image
+              alt="To.Letter Start!"
+              width={72}
+              height={72}
+              src={"/images/guideStart.png"}
+            />
+          </MoveRootPage>
+        )}
+        {progress < 100 && (
+          <Image
+            alt="To.Letter Wait.."
+            width={72}
+            height={72}
+            src={"/images/guideWait.png"}
+          />
+        )}
+      </ElementBox>
+    </Overlay>
+  );
+}
+
+// 스피너 애니메이션
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+// 스피너가 표시될 때 화면을 어둡게 덮는 오버레이
+const Overlay = styled(SectionBox)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+`;
+
+// 스피너 자체
+/*
+Copyright (c) 2023 - WrmYT0bxW0 - https://codepen.io/WrmYT0bxW0/pen/WNeyPj
+
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall 
+be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+*/
+const Spinner = styled.div`
+  position: fixed;
+  width: 34px;
+  height: 34px;
+  top: 64px;
+  left: 56px;
+  margin-left: -17px;
+  margin-top: -17px;
+
+  &:before {
+    content: "";
+    position: fixed;
+    border: 4px solid white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 48px;
+    height: 48px;
+    margin-left: -8px;
+    margin-top: -18px;
+    animation: ${spin} 2.5s linear infinite;
+    animation-direction: reverse;
+  }
+`;
+
+const MoveRootPage = styled(ElementBox)`
+  cursor: pointer;
+`;
