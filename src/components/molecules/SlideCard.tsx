96f712c9ab9442d28ae4270d6920f3c893cd1c6f
JeongYunMi <tamd5971@gmail.com>
2025-01-17 13:03:19 +0900

    fix: slideCard css 조정 #66

--- src/components/molecules/SlideCard.tsx
+++ src/components/molecules/SlideCard.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function SlideCard() {
   return (
-    <main className="w-full h-full flex rounded-sm border-2 border-solid border-zinc-700 ">
+    <main className="w-full h-full flex rounded-sm border-1 border-solid border-zinc-700 ">
       <Image
         width={540}
         height={600}
         src={"/images/guide/roomDesk.png"}
         alt="room-desk-chair"
       />
-      <section className="w-[540px] h-[600px] bg-[#eedbcf] border-s-zinc-700 p-[24px]">
+      <section className="w-[540px] h-[600px] bg-[#eedbcf] border-s-2 border-s-solid border-s-zinc-400 p-[24px]">
         <div className="w-full h-[60px] border-b-2 border-b-solid border-b-zinc-700 flex justify-start items-center">
           <h1 className="text-2xl font-bold">login, signup</h1>
         </div>
--- src/components/organisms/guide/Slide.tsx
+++ src/components/organisms/guide/Slide.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Slide() {
   return (
     <div className="w-screen h-screen bg-[#7b5d54] flex justify-center items-center">
-      <div w-1080 h-600>
+      <div className="w-1080 h-600 shadow-lg">
         <SlideCard />
       </div>
     </div>
