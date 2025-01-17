32d0949211bf44b5d083594b38e84edff3f5c7d3
JeongYunMi <tamd5971@gmail.com>
2025-01-17 12:45:30 +0900

    feat: 시작 안내 페이지 시작 초안 #66

---
+++ src/components/organisms/guide/Slide.tsx
@@ -0,0 +1,12 @@
+import SlideCard from "@/components/molecules/SlideCard";
+import React from "react";
+
+export default function Slide() {
+  return (
+    <div className="w-screen h-screen bg-[#7b5d54] flex justify-center items-center">
+      <div w-1080 h-600>
+        <SlideCard />
+      </div>
+    </div>
+  );
+}
