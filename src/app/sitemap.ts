252a8cc1f353961947e8a84e7666061ab622110f
JeongYunMi <tamd5971@gmail.com>
2025-01-19 19:11:17 +0900

    perf: /guide 페이지 sitemap 작성 #88

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 1,
     },
+    {
+      url: "https://to-letter-front.vercel.app/guide",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
   ];
 }
