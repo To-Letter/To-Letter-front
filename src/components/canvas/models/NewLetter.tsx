f74778ef6431d88a27ce6e8e3897048beb3fd4cc
JeongYunMi <tamd5971@gmail.com>
2025-01-20 21:55:58 +0900

    feat: 안 읽은 메일함 이동 #75

--- src/components/canvas/models/NewLetter.tsx
+++ src/components/canvas/models/NewLetter.tsx
@@ -5,14 +5,15 @@ import { useEffect } from "react";
 import { MeshStandardMaterial } from "three";
 import * as THREE from "three";
 import { useGLTFLoader } from "@/hooks/useGLTFLoader";
+import { useRouter } from "next/navigation";
 /* import { useSetRecoilState } from "recoil"; */
 /* import { newLetterPopupState } from "../../recoil/newLetterPopupState"; */
 
 const NewLetter = () => {
   /** 새로운 편지 glb모델 */
   const LetterGlb = useGLTFLoader("/models/letter.glb");
-  /** 새로운 편지 모델 상태 recoil */
-  /*   const setNewLetterPopup = useSetRecoilState(newLetterPopupState); */
+  /** 새로운 편지함 path이동 */
+  const router = useRouter();
 
   /** 편지 모델 텍스처 */
   const letterTexture = useLoader(
@@ -43,7 +44,7 @@ const NewLetter = () => {
       position={[-0.5, -1.97, -2.5]}
       castShadow
       receiveShadow
-      /*       onClick={() => setNewLetterPopup(true)} */
+      onClick={() => router.push("/letter/newletter")}
     >
       <primitive object={LetterGlb.scene} />
     </mesh>
