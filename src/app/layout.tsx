ab1192ca9979cc4330c9e0e9d1867211e30cfe1c
JeongYunMi <tamd5971@gmail.com>
2025-01-22 13:22:23 +0900

    fix: 콘솔 확인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { memo } from "react";
 import ClientProviders from "@/components/ClientProviders";
 import "@/style/globals.css";
 import KakaoScript from "@/components/KakaoScript";
+import { headers } from "next/headers";
 
 export const metadata: Metadata = {
   title: "TO. Letter",
@@ -60,6 +61,9 @@ const RootLayout = memo(
     children: React.ReactNode;
     modal: React.ReactNode;
   }) => {
+    const token = headers().get("authorization");
+    console.log("token", token);
+
     return (
       <html lang="ko">
         <head>
