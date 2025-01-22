efa16a8d9d998a665a13607aee58e780ca8ef26c
JeongYunMi <tamd5971@gmail.com>
2025-01-22 13:17:48 +0900

    fix: 미들웨어 토큰 값 감지 방법 변경 #87

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
-/* import { NextResponse } from "next/server"; */
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   console.log("pathname", pathname);
   // Authorization 헤더 또는 쿠키에서 토큰 확인
-  const token = request.headers.get("Authorization")?.replace("Bearer ", "");
+  const token = headers().get("authorization");
   console.log("request", request);
   console.log("token", token);
   // 토큰이 없으면
-  /* if (!token) {
+  if (token === null) {
     // URL에 현재 시도한 경로를 state로 포함
     const url = new URL("/", request.url);
     url.searchParams.set("showLogin", "true");
 
     return NextResponse.redirect(url);
-  } */
+  }
 }
 
 export const config = {
