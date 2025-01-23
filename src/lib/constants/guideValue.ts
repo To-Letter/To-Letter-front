e108f21c214745160d058149aea9ee6d2cb67a28
JeongYunMi <tamd5971@gmail.com>
2025-01-23 12:02:06 +0900

    refactor: /guide Page 이미지 파일 확장자 png->webp 변경 #87

--- src/lib/constants/guideValue.ts
+++ src/lib/constants/guideValue.ts
@@ -14,21 +14,21 @@ interface guideValueI {
 export const guideValue: guideValueI[] = [
   {
     imgAlt: "room-desk-chair",
-    imgSrc: "/images/guide/roomDesk.png",
+    imgSrc: "/images/guide/roomDesk.webp",
     guideHeader: "[Guest] 로그인, 회원가입",
     guideContents:
       "책상 앞 의자를 클릭하여 계정을 만들고 로그인을 할 수 있어요.",
   },
   {
     imgAlt: "room-bed",
-    imgSrc: "/images/guide/roomBed.png",
+    imgSrc: "/images/guide/roomBed.webp",
     guideHeader: "[User] 마이페이지",
     guideContents: `책상 왼쪽으로 시점을 돌린 후 침대를 클릭하여 마이페이지에 들어갈 수 있어요.\n
       마이페이지에서는 내 정보를 수정하거나, 비밀번호 변경(카카오 로그인이 아닐 경우), 회원 탈퇴 기능을 지원해요.`,
   },
   {
     imgAlt: "room-desk",
-    imgSrc: "/images/guide/roomDeskUser.png",
+    imgSrc: "/images/guide/roomDeskUser.webp",
     guideHeader: "[User] 편지함, 편지 쓰기, 내 편지 주소 공유",
     guideContents: `책상 위 오른쪽 연필꽂이를 클릭하여 편지를 작성할 수 있어요(상대방의 편지 주소를 알고 있어야 해요.).\n
       책상 중간 책을 클릭하여 받은 편지 목록과 보낸 편지 목록을 확인할 수 있어요(개별 편지 열람이 가능해요.).\n
@@ -36,7 +36,7 @@ export const guideValue: guideValueI[] = [
   },
   {
     imgAlt: "room-Trash",
-    imgSrc: "/images/guide/roomTrash.png",
+    imgSrc: "/images/guide/roomTrash.webp",
     guideHeader: "[User] 편지 삭제",
     guideContents: `책상 오른쪽에 위치한 쓰레기통을 클릭하여, 받은 편지와 보낸 편지를 삭제할 수 있어요.`,
   },
